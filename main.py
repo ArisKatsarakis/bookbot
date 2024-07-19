06c9672672f7118db139245916738d2163aeae64
ArisKatsarakis <ariskatsarakis@hotmail.com>
2024-07-19 14:12:30 +0300

    add count_words read_file character_found functions

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-books_dicretory = './books/frankenstein.txt'
-with open(books_dicretory) as f:
-    file_contents = read()
-    words = file_contents.split()
+def read_file(): 
+    books_dicretory = './books/frankenstein.txt'
+    with open(books_dicretory) as f:
+        file_contents = f.read()
+        return file_contents
+def character_found(file):
+    chars_dictionary = { }
+    for i in range(97, 123):
+        chars_dictionary[chr(i)] = 0
+    for x in file: 
+        if (ord(x)>=97 and ord(x)<123) or (ord(x) >=65  and ord(x)< 89):
+            x = x.lower()
+            chars_dictionary[x] = chars_dictionary[x] + 1 
+    return chars_dictionary
+
+
+def count_words(file):
+    return file.split()
+
+if __name__ == "__main__":
+    print(character_found(read_file()))
